8002f0d4df24f82ca9221c01d888bead11b245c4
saadmannur <saadmannur99@gmail.com>
2026-06-22 17:01:59 +0600

    my listing data getting

--- src/app/(dashboard)/my-listing/page.jsx
+++ src/app/(dashboard)/my-listing/page.jsx
@@ -1,6 +1,19 @@
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import React from 'react';
 
-const MyListingPage = () => {
+const MyListingPage = async () => {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    const user = session?.user;
+
+    const res = await fetch(`http://localhost:5000/pet/email/${user?.email}`);
+    const data = await res.json()
+    console.log(data)
+
+
     return (
         <div>
             this is my listing page
